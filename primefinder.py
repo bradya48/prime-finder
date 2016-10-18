e5a9cd839f0108ab7603d9b6b441177a5a903151
bradya48 <bradya48@gmail.com>
2016-10-18 11:27:39 -0500

    Update primefinder.py

--- primefinder.py
+++ primefinder.py
@@ -1,20 +1,25 @@
-prime_list = [] # initialize list that contains primes
+import time
+import math
+
+runtime_list = []
+max_search = int(raw_input("Max value? ")) # ask user for max value that program will search for
 
 def isprime(x):
+    sqrt_x = math.sqrt(x) # find square root of x
     for number in prime_list: # go through all values in prime_list
         if x % number == 0: # if number is divisible by prime in prime_list, exit
             return False
+        elif number > sqrt_x:
+            return True # if items in list are greater than square root, must be prime
 
-    return True # if no numbers are divisible, return True
+start_time = time.time() # time at start of loop
 
-
-max_search = int(raw_input("Max value? ")) # ask user for max value that program will search for
-
-
+prime_list = [] # initialize list that contains primes
 prime_list.append(2) # start by initializing 2 as prime number
-print 2
+    
 
-for i in range(3,max_search + 1):
+for i in range(3,max_search + 1,2):
     if isprime(i):
         prime_list.append(i) # if i is prime, add i to list
-        print i # print i if prime
+
+print 'Runtime: ' + str((time.time() - start_time))
