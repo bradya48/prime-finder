b0cb1eafa3d78eb8861c1294cdae750d3de0aca4
bradya48 <bradya48@gmail.com>
2016-10-17 22:02:09 -0500

    Update primefinder.py

--- primefinder.py
+++ primefinder.py
@@ -1,23 +1,20 @@
-import sys
-
-prime_list = [2]
+prime_list = [] #initialize list that contains primes
 
 def isprime(x):
     for number in prime_list:
-        if x % number == 0:
+        if x % number == 0: # if number is divisible by prime in prime_list, exit
             return False
 
-    return True
+    return True # if no numbers are divisible, return True
 
 
-max_search = int(raw_input("Max value? "))
+max_search = int(raw_input("Max value? ")) # ask user for max value that program will search for
 
 
-prime_list.append(2)
+prime_list.append(2) # start by initializing 2 as prime number
 print 2
 
-for i in range(2,max_search):
+for i in range(3,max_search):
     if isprime(i):
-        prime_list.append(i)
+        prime_list.append(i) #if isprime returns True, add i to list
         print i
-        
